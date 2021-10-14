4f5fe455ff5d97f7396f4f00c5caffe0449ef503
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2021-10-14 15:06:54 +0900

    Created Footer.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { Header } from '@/components/common/Header';
+import { Footer } from '@/components/common/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
